92c4e74dd6485855f828af984983ae0eb8544b02
HaseebAHMED-afk <haseeb.ahmed456123@gmail.com>
2021-04-08 22:34:30 +0500

    Main ui done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
+import { Typography } from '@material-ui/core';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      Hello
+     <Typography variant='h3' style={{color:'white'}}  >MERN String reverser</Typography>
+     <Typography variant='body1' style={{color:'white'}} >By <a href='https://www.github.com/HaseebAHMED-afk' > HaseebAHMED-afk</a></Typography>
+     <div className='input-section' >
+       <label>Enter something: <input className='string-input' type='text' ></input></label>
+       <button className='submit-btn' >Submit</button>
+     </div>
     </div>
   );
 }
---
+++ src/Components/StringCard.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Card.css'
+
+const StringCard = () => {
+    return (
+        <div className='card' >
+            <p>Input:  </p>
+            <p>Output:</p>
+            <p>Length:</p>
+        </div>
+    )
+}
+
+export default StringCard
